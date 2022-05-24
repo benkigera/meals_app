2ace7ed5450d88cd2d05e2b444a001295bbddce7
Benson Kigera <benkigera@gmail.com>
2022-05-24 12:14:44 +0300

    Initial commit

---
+++ lib/widgets/categories_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/category_item.dart';
+import 'package:meals_app/models/dummy_data.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meal App'),
+      ),
+      body: GridView(
+
+        //returns a category item as widgets
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
