d405f17bb8be032e2e5a5cc3c5ff46cd743e529d
Benson Kigera <benkigera@gmail.com>
2022-05-25 10:33:25 +0300

    Populate meal detail page

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -21,7 +21,7 @@ class CategoryMealScreen extends StatelessWidget {
       body: ListView.builder(
         itemBuilder: (ctx, index) {
           return MealItem(
-            id: categoryMeals[index].title,
+            id: categoryMeals[index].id,
             title: categoryMeals[index].title,
             imageUrl: categoryMeals[index].imageUrl,
             duration: categoryMeals[index].duration,
