c1b7a6b23cc19b3a03a95b4c17bb9c134e123cb6
Benson Kigera <benkigera@gmail.com>
2022-05-25 10:49:14 +0300

    Add a TabController

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -3,33 +3,26 @@ import 'package:meals_app/widgets/category_item.dart';
 import 'package:meals_app/models/dummy_data.dart';
 
 class CategoryScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meal App'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
+    return GridView(
+      padding: const EdgeInsets.all(25),
 
-        //returns a category item as widgets
-        children: DUMMY_CATEGORIES
-            .map(
-              (catData) => CategoryItem(
-                catData.id,
-                catData.title,
-                catData.color,
-              ),
-            )
-            .toList(),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+      //returns a category item as widgets
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.id,
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
